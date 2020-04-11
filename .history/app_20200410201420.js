12e9fd04b3ae87eefe6e8520c1a3fc7242e8388b
jnnnn1999 <jnnnn1999@gmail.com>
2020-04-11 16:53:36 +0700

    2

---
+++ .history/app_20200410201420.js
@@ -0,0 +1,114 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator =require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const app = express();
+// mongoose.connect(config.database);
+// let db = mongoose.connection;
+
+
+
+
+
+//MongoDB
+const mongo = require('./routes/articles')
+app.use(mongo)
+
+// Check connection
+// const db = mongoose.connection
+// db.on('error', () => console.log('Database connection error'))
+// db.once('open', () => console.log('Database connected'))
+
+
+// Bring in Models
+let Article = require('./models/article');
+//local View Engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// parse application/json
+app.use(bodyParser.json())
+
+//Set Public Floder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Passport config
+require('./config/passport')(passport);
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
+
+//Home Route
+app.get('/', function(req,res){
+  Article.find({}, function(err,articles){
+    if(err){
+      console.log(err);
+    }else{
+      res.render('home',{
+        title:'Articles',
+        articles: articles
+
+      });
+    }
+  })
+});
+
+//Route Files
+let articles =require('./routes/articles');
+let users =require('./routes/users');
+let profiles =require('./routes/profiles');
+
+app.use('/articles', articles);
+app.use('/users', users);
+
+//Start Server
+app.listen(2000,function(){
+  console.log('Server started on port 2000...')
+})
---
+++ .history/routes/users_20200411013620.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+// Bring in User Model
+let User = require('../models/user');
+
+// Register Form
+router.get('/register', function(req, res){
+  res.render('register');
+});
+// Register Proccess
+router.post('/register', function(req, res){
+  name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors:errors
+    });
+  } else {
+    let newUser = new User({
+      name:name,
+      email:email,
+      username:username,
+      password:password
+    });
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success','You are now registered and can log in');
+            res.redirect('/users/login');
+          }
+        });
+      });
+    });
+  }
+});
+
+//Load Edit Profile Form
+router.get('/editpp/:id', ensureAuthenticated, function(req,res){
+  res.render('edit_profile',{
+    title: 'Edit Profile'
+
+  });
+});
+
+
+//Update Sumit Post Route
+router.post('/editpp/:id',function(req, res){
+let article = {};
+article.title = req.body.title;
+article.author = req.body.author;
+article.body = req.body.body;
+
+let query ={_id:req.params.id}
+
+Article.update(query, article,function(err){
+  if(err){
+    console.log(err);
+    return;
+  }else{
+    req.flash('success','Profile Updated');
+    res.redirect('/');
+  }
+});
+});
+
+// Login Form
+router.get('/login', function(req, res){
+  res.render('login');
+});
+
+// Login Process
+router.post('/login', function(req, res, next){
+  passport.authenticate('local', {
+    successRedirect:'/',
+    failureRedirect:'/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// logout
+router.get('/logout', function(req, res){
+  req.logout();
+  req.flash('success', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
